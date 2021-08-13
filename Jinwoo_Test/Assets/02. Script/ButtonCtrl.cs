f86c634a6d662a1f8e2638c1549bcd9a2a3516fd
wlsdn0317 <wlsdn0317@naver.com>
2021-08-13 12:43:48 +0900

    210813

--- Jinwoo_Test/Assets/02. Script/ButtonCtrl.cs
+++ Jinwoo_Test/Assets/02. Script/ButtonCtrl.cs
@@ -9,40 +9,61 @@ public class ButtonCtrl : MonoBehaviour
     public CharacterController playerCtrl;
     public List<GameObject> wayPoints = new List<GameObject>();
 
+    float recognitionRange;
+
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("PLAYER");
         wayPoints.AddRange(GameObject.FindGameObjectsWithTag("WAYPOINT"));
-        playerCtrl = player.GetComponent<CharacterController>();
-
+        recognitionRange = 2.5f;
     }
 
     public void OnClickHuman()
     {
+        StartFade(0);
         playerCtrl.enabled = false;
-        player.transform.position = wayPoints[0].transform.position + Vector3.up*5f;
-        playerCtrl.enabled = true;
     }
+
     public void OnClickElven()
     {
+        StartFade(1);
         playerCtrl.enabled = false;
-        player.transform.position = wayPoints[1].transform.position + Vector3.up*5f;
-        playerCtrl.enabled = true;
     }
+
     public void OnClickGoblin()
     {
+        StartFade(2);
         playerCtrl.enabled = false;
-        player.transform.position = wayPoints[2].transform.position + Vector3.up*5f;
-        playerCtrl.enabled = true;
     }
-    public void OnClickUndead() 
+
+    public void OnClickUndead()
     {
+        StartFade(3);
         playerCtrl.enabled = false;
-        player.transform.position = wayPoints[3].transform.position + Vector3.up*5f;
-        playerCtrl.enabled = true;
     }
+
     public void OnClickExit()
     {
         GameManager.instance.wayPointUI.SetActive(false);
+    }
+
+    public void moveWaypoint(int _waynumber)
+    {
+        //플레이어가 서있는 웨이포인트와 이동하려는 웨이포인트 거리가 일정이상 멀어야 작동
+        //플레이어가 서있는 웨이포인트가 가까우면 같은 포인트라고 인식하도록
+        if (Vector3.Distance(player.transform.position, wayPoints[_waynumber].transform.position) > recognitionRange)
+        {
+            player.transform.position = wayPoints[_waynumber].transform.position;
+            playerCtrl.enabled = true;
+        }
+        
+    }
+
+    public void StartFade(int _waynumber)
+    {
+        if (Vector3.Distance(player.transform.position, wayPoints[_waynumber].transform.position) > recognitionRange)
+        {
+            StartCoroutine(UIManager.instance.FadeCoroutine(0.5f,_waynumber));
+        }
     }
 }
--- Jinwoo_Test/Assets/02. Script/PlayerActionCtrl.cs
+++ Jinwoo_Test/Assets/02. Script/PlayerActionCtrl.cs
@@ -7,35 +7,28 @@ public class PlayerActionCtrl : MonoBehaviour
     [SerializeField]
     GameObject wayPointUI;
 
-    public List<GameObject> wayPoints = new List<GameObject>();
-    public List<GameObject> merchants = new List<GameObject>();
-
-    public CharacterController playerCtrl;
-    float recognitionRange; //상호작용 거리 (대화,웨이포인트)
     void Awake()
     {
-        recognitionRange = 2.5f;
     }
     private void Start()
     {
-        wayPoints.AddRange(GameObject.FindGameObjectsWithTag("WAYPOINT"));
-        merchants.AddRange(GameObject.FindGameObjectsWithTag("MERCHANT"));
-        playerCtrl = GetComponent<CharacterController>();
     }
 
     void Update()
     {
         TryAction();
-        CheckDistance();
     }
 
     private void TryAction()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            if (UIManager.instance.hotKeyGuid.activeSelf && wayPoints.Contains(UIManager.instance.hotKeyGuidTarget))
+            if (UIManager.instance.hotKeyGuid.activeSelf)
             {
-                wayPointUI.SetActive(true);
+                if (UIManager.instance.wayPoints.Contains(UIManager.instance.hotKeyGuidTarget))
+                {
+                    wayPointUI.SetActive(true);
+                }
             }
             else
             {
@@ -48,39 +41,7 @@ public class PlayerActionCtrl : MonoBehaviour
         }
     }
 
-    private void CheckDistance()
-    {
-        foreach (GameObject _wayPoint in wayPoints) //웨이포인트 작용 키 활성화
-        {
-            if (Vector3.Distance(transform.position, _wayPoint.transform.position) <= recognitionRange)
-            {
-                if (!wayPointUI.activeSelf)
-                {
-                    UIManager.instance.hotKeyGuid.SetActive(true);
-                    UIManager.instance.hotKeyGuidTarget = _wayPoint;
-                    return;
-                }
-            }
-            else
-            {
-                UIManager.instance.hotKeyGuid.SetActive(false);
-                //wayPointUI.SetActive(false);
-            }
-        }
-        foreach (GameObject _merchant in merchants) //상점 or NPC 상호작용 키 활성화
-        {
-            if (Vector3.Distance(transform.position, _merchant.transform.position) <= recognitionRange)
-            {
-                UIManager.instance.hotKeyGuid.SetActive(true);
-                UIManager.instance.hotKeyGuidTarget = _merchant;
-                return;
-            }
-            else
-            {
-                UIManager.instance.hotKeyGuid.SetActive(false);
-            }
-        }
-    }
+    
 
     
 }
