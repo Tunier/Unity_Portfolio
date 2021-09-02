fa6340e7629ce0cf8d257e0b6a6a08d86594c097
Tunier <krymk1212@naver.com>
2021-09-02 12:07:01 +0900

    21.09.02

---
+++ Asset_Test/Assets/02. Script/Player/PlayerWeaponCtrl.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponCtrl : MonoBehaviour
+{
+    public PlayerInfo player;
+    public GameObject curHitMob;
+    public List<GameObject> mobList = new List<GameObject>();
+
+    Collider col;
+
+    void Awake()
+    {
+        col.enabled = false;
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MONSTER")) //몬스터가 맞으면
+        {
+            curHitMob = other.gameObject;
+
+            player.targetMonster = curHitMob; // 맞은 몬스터 정보를 플레이어에게 전달
+
+            if (!mobList.Contains(other.gameObject)) // 맞은 몬스터가 리스트에 없으면
+            {
+                mobList.Add(other.gameObject); // 맞은 몬스터를 리스트에 저장하고
+
+                if (CritcalCal()) // 크리티컬이 떴는지 계산해서 Hit를 호출
+                {
+                    curHitMob.GetComponent<MonsterBase>().Hit(player.finalAtk * 1.5f);
+                    // 데미지 UI 출력하는 구문 작성해야함. 크리티컬이 뜨면 해당 UI Text의 컬러를 바꿔주는 기능도 추가해야함.
+                }
+                else
+                {
+                    curHitMob.GetComponent<MonsterBase>().Hit(player.finalAtk);
+                }
+            }
+            else { return; }
+        }
+    }
+
+    public bool CritcalCal()
+    {
+        bool isCrit = false;
+        int crit;
+
+        crit = Random.Range(0, 10000);
+
+        if (player.finalCriticalChance >= crit)
+            isCrit = true;
+
+        return isCrit;
+    }
+}
