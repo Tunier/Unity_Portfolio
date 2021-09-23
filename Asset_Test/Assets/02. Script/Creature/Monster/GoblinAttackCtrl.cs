91541a279e3eff8c52a51935ce3fd8a5e734945d
wlsdn0317 <wlsdn0317@naver.com>
2021-09-23 17:53:16 +0900

    21.09.23

---
+++ Asset_Test/Assets/02. Script/Creature/Monster/GoblinAttackCtrl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinAttackCtrl : MonoBehaviour
+{
+    GameObject player;
+    bool isAttacked;
+
+    public float attackDamage = 50f;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(AutoDisable());
+        isAttacked = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!isAttacked)
+            {
+                var playerInfo = player.GetComponent<PlayerInfo>();
+                playerInfo.Hit(attackDamage);
+                isAttacked = true;
+
+                if (playerInfo.curHp <= 0)
+                    playerInfo.Die();
+            }
+        }
+    }
+    IEnumerator AutoDisable()
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        gameObject.SetActive(false);
+    }
+
+}
